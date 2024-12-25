e07e89c9f0ce75681e8b4d84a67421bd8e424279
Guilherme615 <moura.guilherme@escolar.ifrn.edu.br>
2024-12-25 00:17:45 -0300

    .

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="" alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Albums</li>
+        <li>Photos`</li>
+        <li>Comments</li>
+        <li>Posts`</li>
+        <li>User</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
